28606aec65d911390b3c90edff59f6816af97131
Andrew Midthun <midth002@gmail.com>
2022-10-30 09:28:19 -0500

    Added helloworld controller and view.

---
+++ FirstASPMVCApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstASPMVCApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public string Hello()
+        {
+            return "Who's there";
+
+        }
+    }
+}
